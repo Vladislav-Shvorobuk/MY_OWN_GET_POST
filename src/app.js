02863d8f655d191b0979c5454e1b93c3816b92da
Vladislav <vladislav.shvorobuk@gmail.com>
2019-02-03 20:52:39 +0200

    branch refactor: 1. refactor app.js - change names, change block catch();

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const uploadForm = document.getElementById('uploadForm');
 const downloadForm = document.getElementById('downloadForm');
 
 //  UPLOAD FILE
-uploadForm.addEventListener('submit',  event => {
+uploadForm.addEventListener('submit', event => {
   event.preventDefault();
   const [file] = event.target.sampleFile.files;
-  console.log(file);
+
   if (!file) {
     setMessage('CHOOSE_A_FILE');
     return;
@@ -24,34 +24,43 @@ uploadForm.addEventListener('submit',  event => {
       setMessage('SUCCES_DOWNLOAD_FILE');
     })
     .catch(error => {
-      setMessage('ERROR', error);
+      uploadProgressBar.classList.remove('visible');
+      uploadProgressValue.classList.remove('visible');
+      setMessage('ERROR', `${error}`);
     });
 });
 
 //  DOWNLOAD FILE
 downloadForm.addEventListener('submit', event => {
   event.preventDefault();
-  const nameOfDownloadFile = document.querySelector('.nameOfDownloadFile').value;
+  const fileName = nameOfDownloadFile.value;
 
-  if (!nameOfDownloadFile) {
+  if (!fileName) {
     setMessage('CHOOSE_A_FILE');
     return;
   }
 
   const response = new HttpRequest({ baseUrl: 'http://localhost:8000' });
 
-  response.get(`/files/${nameOfDownloadFile}`, { responseType: 'blob', onDownloadProgress })
+  response.get(`/files/${fileName}`, { responseType: 'blob', onDownloadProgress })
     .then(data => {
       if (IMAGE_MIME_TIPES.includes(data.type)) {
         viewImage(data);
         setMessage('SUCCES_VIEW_IMAGE');
       } else {
-        downloadFile(data, nameOfDownloadFile);
+        downloadFile(data, fileName);
         setMessage('SUCCES_DOWNLOAD_FILE');
       }
     })
     .catch(error => {
-      setMessage('NOT_EXIST');
+      downLoadProgressBar.classList.remove('visible');
+
+      if ((/[404]/).test(error)) {
+        setMessage('NOT_EXIST');
+        return;
+      }
+
+      setMessage('ERROR', `${error}`);
     });
 });
 
